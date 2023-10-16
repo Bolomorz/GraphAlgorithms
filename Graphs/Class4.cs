3e737ff17016af05168e55d0af959e3a7b66caf2
Dominik Schneider <Dominik.Schneider@wibizu.de>
2023-10-16 16:22:35 +0200

    interface graph

--- Graphs/Class4.cs
+++ Graphs/Class4.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Graphs
 {
-    public class Graph
+    public interface IGraph
+    {
+        int GetVertexIndex(Vertex ver);
+        void AddVertex(Vertex ver);
+        int GetEdgeIndex(Edge edg);
+        bool IsEdgeInGraph(Edge edg);
+        void AddEdge(Edge edg);  
+        void RemoveEdge(Edge edg);
+        void RemoveVertex(Vertex ver);
+
+    }
+    public class Graph : IGraph
     {
         public List<Vertex> vertices {  get; set; }
         public List<Edge> edges { get; set; }
@@ -136,17 +147,58 @@ namespace Graphs
         }
     }
 
-    public class DirectedGraph : Graph
+    public class DirectedGraph : IGraph
     {
+        public List<Vertex> vertices { get; set; }
+        public List<Edge> edges { get; set; }
+        protected static int num = 0;
+        public int id { get; set; }
+
         public DirectedGraph()
         {
             vertices = new List<Vertex> ();
             edges = new List<Edge> ();
-            id = Graph.num;
-            Graph.num++;
+            id = DirectedGraph.num;
+            DirectedGraph.num++;
         }
 
-        public new bool IsEdgeInGraph(Edge edg)
+        public int GetVertexIndex(Vertex ver)
+        {
+            int index = 0;
+            foreach (var v in vertices)
+            {
+                if (v == ver)
+                {
+                    return index;
+                }
+                index++;
+            }
+            return -1;
+        }
+
+        public void AddVertex(Vertex ver)
+        {
+            if (GetVertexIndex(ver) == -1)
+            {
+                vertices.Add(ver);
+            }
+        }
+
+        public int GetEdgeIndex(Edge edg)
+        {
+            int index = 0;
+            foreach (var e in edges)
+            {
+                if (e == edg)
+                {
+                    return index;
+                }
+                index++;
+            }
+            return -1;
+        }
+
+        public bool IsEdgeInGraph(Edge edg)
         {
             foreach (var e in edges)
             {
@@ -158,7 +210,7 @@ namespace Graphs
             return false;
         }
 
-        public new void AddEdge(Edge edg)
+        public void AddEdge(Edge edg)
         {
             if (GetEdgeIndex(edg) == -1)
             {
@@ -170,6 +222,57 @@ namespace Graphs
                     edg.v1.AddAdjacent(edg.v2);
                 }
             }
+        }
+
+        public void RemoveEdge(Edge edg)
+        {
+            int index = GetEdgeIndex(edg);
+            if (index != -1)
+            {
+                edges.RemoveAt(index);
+            }
+        }
+
+        public void RemoveVertex(Vertex ver)
+        {
+            int index = GetVertexIndex(ver);
+            if (index != -1)
+            {
+                foreach (var e in edges)
+                {
+                    if (e.v1 == ver || e.v2 == ver)
+                    {
+                        RemoveEdge(e);
+                    }
+                }
+                vertices.RemoveAt(index);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "graph";
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 17;
+            foreach (var ver in vertices)
+            {
+                hash += ver.GetHashCode();
+            }
+            foreach (var edg in edges)
+            {
+                hash += edg.GetHashCode();
+            }
+            return hash;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || typeof(Graph) != obj.GetType()) return false;
+            Graph objGraph = (Graph)obj;
+            if (id == objGraph.id) return true; else return false;
         }
     }
 }
